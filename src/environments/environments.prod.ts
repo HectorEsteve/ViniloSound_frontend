95f8e2181f40d1ae279c5de8b7bd755a95376e49
HectorEsteve <hectorestevex2@gmail.com>
2024-06-03 16:47:45 +0200

    detalles del docker

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,6 +1,6 @@
 export const environments ={
-  //baseUrl: 'http://localhost:8000/api',
-  baseUrl: 'vinilosound.back.mooo.com:8000/api',
+  baseUrl: 'http://localhost:8000/api',
+  //baseUrl: 'http://vinilosound.back.mooo.com:8000/api',
   tempRoutSong: '',
   tempRoutGenre: '',
   tempRoutBand: '',
